e4fffe596c563d047ba8ab590e3254c138a3b41e
vanousek74 <d.a.n.n.y74@seznam.cz>
2025-07-26 14:05:50 +0200

    Add files via upload

---
+++ wh-balancer.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         WH Balancer by vanousek74
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Automatický balancer surovin pro Divoké Kmeny s volitelným filtrováním a prioritami.
+// @author       vanousek74
+// @match        https://*.divokekmeny.cz/game.php?*screen=overview_villages*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    alert("Skript byl úspěšně načten! Funkce budou přidány v další verzi.");
+})();
